46772904d737bfa314e6068136c9c14d6d4e7802
laylabtrice <layla.beatrice@alunoescolassociais.org.br>
2024-09-11 09:32:00 -0300

    Construção cod. de index, routes e AuthController

---
+++ controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+//Requer arquivo user que contém model user com aa funções de manipulação de dados dos usuários
+require_once 'models/user.php';
+
+class AuthController
+{
+    // Função responsável processo de login
+    public function login ()
+    {
+        // Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
+
+    }
+}
+?>
