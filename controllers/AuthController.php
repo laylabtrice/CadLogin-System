bd0bba327ff555a2b9b9bbbbb65c4e2e67a11939
laylabtrice <layla.beatrice@alunoescolassociais.org.br>
2024-09-24 11:09:21 -0300

    códigos em AuthController, user e index

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -9,6 +9,27 @@ class AuthController
     {
         // Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
 
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+            //Obter valores do form
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+
+            //Chama o método do model para encontrar o user por email
+
+            $user = User::findByEmail($email);
+
+            if($user && password_verify($senha, $user['senha'])){
+                //Verifica se a senha corresponde a um hash
+                session_start();
+                //Armazena na sessão o ID do usuário que está logado e seu perfil
+
+                $_SESSION['usuario_id'] = $user['id'];
+                $_SESSION['perfil'] = $user['perfil'];
+                header('');
+            }
+        }
     }
+
 }
 ?>
