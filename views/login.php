87c226fa15ffe52644726f9ab60566f30ee46c41
laylabtrice <layla.beatrice@alunoescolassociais.org.br>
2024-09-25 09:57:35 -0300

    Estilização de páginas

--- views/login.php
+++ views/login.php
@@ -4,8 +4,7 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
     <title>Login</title>
 </head>
 <body>
@@ -21,7 +20,7 @@
             </section>
             <button type="submit">Login</button>
         </form>
-        <a href="">Cadastre-se</a>
+        <a href="index.php?action=register">Cadastre-se</a>
     </main>
 </body>
 </html>
